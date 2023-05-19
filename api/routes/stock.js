edce74cd9c78970d4f1b7629c27f98817762f95d
RishiRewadkar <rishirewadkar@gmail.com>
2023-05-19 03:07:04 -0700

    Rank and Networth pending

--- api/routes/stock.js
+++ api/routes/stock.js
@@ -1,17 +1,3 @@
-/*
-get stock data api show live stock data of all the stocks 
-
-buy order 
-
-sell order
-
-rank 
-
-net worth 
-
-*/
-
-
 const express = require('express');
 const router = express.Router();
 const Student= require('../models/student');
@@ -54,72 +40,101 @@ router.get('/current', (req,res,next)=>{
 
    
 router.get('/buy', (req,res,next)=>{
-   let price = 100
+  const API_KEY = 'SPCBTAYD6UU1R0MU'; // replace with your own API key from Alpha Vantage
+  const SYMBOL = 'IBM';
+  
+  const url = `https://api.twelvedata.com/price?symbol=IBM&apikey=759d05c0de694a42977fb5be514be76d`;
+  let price = 100
+
+
+  axios.get(url)
+    .then(response => {
+        console.log("IN here now ")
+        console.log(response.data);
+        price = response.data.price
+        Student.findOne({name: req.body.name})
+        .exec()
+        .then(user =>{
+          console.log(user.amount);
+          if (user.amount < price * req.body.quantity )
+          {
+              console.log("Cant buy inssuficient funds");
+              res.send("Cant Buy Insifficient Funds")
+          }
+          else
+          {
+     
+           console.log("sufficient funds");
+           
+           Student.updateOne({ name: req.body.name }, { $set: { amount: user.amount - price * req.body.quantity , IBM: user.IBM + req.body.quantity } })
+           .exec()
+           .then(result => {
+             console.log(result);
+             res.send("Bought IBM Shares")
+           })
+           .catch(err => {
+             console.error(err);
+           });
+     
+     
+          }
+            })
+            .catch(err=>{
+              res.status(500).json({
+                  err: "I failed here 1"
+              })
+          })
+
+ 
+    })
+    .catch(error => {
+        console.log('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    });
+
+
+
    console.log(req.body)
-   Student.find({name: req.body.name})
-   .exec()
-   .then(user =>{
-     console.log(user[0].amount);
-     if (user[0].amount < price * req.body.quantity )
-     {
-         console.log("Cant buy inssuficient funds");
-     }
-     else
-     {
-
-      console.log("sufficient funds");
-      
-      Student.updateOne({ name: req.body.name }, { $set: { amount: user[0].amount - price * req.body.quantity , IBM: req.body.quantity } })
-      .exec()
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-
-
-
-     }
-       })
-       .catch(err=>{
-         res.status(500).json({
-             err: "I failed here 1"
-         })
-     })
- console.log('buy')
+  
+ console.log('buy Is Here ')
 
  
 })
 
 router.get('/sell', (req,res,next)=>{
+  const API_KEY = 'SPCBTAYD6UU1R0MU'; // replace with your own API key from Alpha Vantage
+  const SYMBOL = 'IBM';
+  
+  const url = `https://api.twelvedata.com/price?symbol=IBM&apikey=759d05c0de694a42977fb5be514be76d`;
+  let Sellingprice = 100
 
-   let price = 100
 
-   console.log(req.body)
-   Student.find({name: req.body.name})
-   .exec()
+  axios.get(url)
+    .then(response => {
+        console.log("IN here now ")
+        console.log(response.data);
+        Sellingprice = response.data.price
+        Student.findOne({name: req.body.name}).exec()
    .then(user =>{
-     console.log(user[0].IBM);
-     if (user[0].IBM < req.body.IBM )
+     console.log(user.IBM);
+     if (user.IBM < req.body.quantity )
      {
          console.log("dont have enough shares to sell");
+         res.send("Cant Place sell order insufficient Shares")
      }
      else
      {
 
       console.log("sufficient shares");
-      Student.updateOne({ name: req.body.name }, { $set: { amount: user[0].amount + price * req.body.IBM , IBM: user[0].IBM-req.body.IBM } })
+      Student.updateOne({ name: req.body.name }, { $set: { amount: user.amount + Sellingprice * req.body.quantity , IBM: user.IBM-req.body.quantity } })
       .exec()
       .then(result => {
         console.log(result);
+        res.send("Placed Sell Order")
       })
       .catch(err => {
         console.error(err);
       });
-
-
 
 
      }
@@ -129,12 +144,13 @@ router.get('/sell', (req,res,next)=>{
              err: "I failed here 1"
          })
      })
- console.log('Sell')
 
 
-
-
-
+    })
+    .catch(error => {
+        console.log('Error fetching data:', error.message);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    });
 
 
 })
@@ -142,18 +158,69 @@ router.get('/sell', (req,res,next)=>{
 
 
 router.get('/rank', (req,res,next)=>{ 
-  let price = 100
+
+  const API_KEY = 'SPCBTAYD6UU1R0MU'; // replace with your own API key from Alpha Vantage
+  const SYMBOL = 'IBM';
+  
+  const url = `https://api.twelvedata.com/price?symbol=IBM&apikey=759d05c0de694a42977fb5be514be76d`;
+  axios.get(url)
+  .then(response => {
+      console.log("IN here now ")
+      console.log(response.data);
+      const stockPrice = response.data.price;
+
+    Student.aggregate([
+        {
+          $addFields: {
+            StockPrice: stockPrice,
+            totalStockAndAssets: { $sum: ["$amount", { $multiply : ["$IBM" , "$StockPrice"] } ] }
+          }
+        },
+      {
+        $sort: {
+          totalStockAndAssets: 1
+        }
+      }
+    ]
+  )
+  .exec()
+  .then(user =>{
+    console.log(user);
+    res.send(user)
+      })
+      .catch(err=>{
+        res.status(500).json({
+            err: "I failed here 1"
+        })
+    })
+  })
+.catch(error => {
+    console.log('Error fetching data:', error.message);
+    res.status(500).json({ error: 'Failed to fetch data' });
+})
 
 
 })
 
 
 router.get('/networth', (req,res,next)=>{ 
+  price = 100
+  console.log(req.body)
+  Student.findOne({name: req.body.name})
+  .exec()
+  .then(user =>{
+    console.log(user);
+    
+    
+    
+      })
+      .catch(err=>{
+        res.status(500).json({
+            err: "I failed here 1"
+        })
+    })
+
 
 })
-
-   
-
-
 
    module.exports = router;
