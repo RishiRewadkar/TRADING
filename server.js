eb609ef41486111a9338dcfcb0b1062d15a1b592
RishiRewadkar <rishirewadkar@gmail.com>
2023-05-23 07:18:10 -0700

    Ready to Deploy

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-/*
-https://www.youtube.com/watch?v=kvFQHgHZx4g&list=PLgWjD_CBfh0ACkCyg5Kjk0mh2Mcc-sZa3&index=4&ab_channel=SBSonlineclasses
-*/
+
 const http = require('http');
 const app = require('./app')
 const server = http.createServer(app);
 const path = require('path')
 
+const port = process.env.PORT || 800
 
-
-server.listen(3000, console.log('app is UP'));
+server.listen(port, console.log('app is UP'));
